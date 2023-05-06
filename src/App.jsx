81b13e26bbe5fae59d1eee4dc78912acba45e9e0
Ankit Kumar <akg95469@gamil.com>
2023-05-06 23:41:48 +0530

    Ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 /* eslint-disable react/prop-types */
-import { useEffect, useState } from "react";
+/* Use of ternary operator */
+//import { useEffect, useState } from "react";
 import "./App.css";
-
-function App() {
-  const [datalist, setdatalist] = useState(null);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setdatalist(data));
-  }, []);
+function DataMap() {
+  const isLoggedIn = false;
+  const profile = [
+    { Name: "Ak", skills: "FullStack Developer" },
+    {
+      Name: "PK",
+      skills: "Frontend Developer",
+    },
+  ];
+  console.log(profile);
   return (
     <>
-      {datalist &&
-        datalist.map((obj) => {
-          return (
-            <ul key={obj.id}>
-              <li>{obj.id}</li>
-              <li>{obj.body}</li>
-              <li>{obj.title}</li>
-            </ul>
-          );
-        })}
+      {isLoggedIn ? (
+        "Back"
+      ) : (
+        <>
+          {profile.map((data) => {
+            console.log(data);
+            (<p>{data.Name}</p>),
+              (
+                <div>
+                  <p>{data.skills}</p>
+                </div>
+              );
+          })}
+        </>
+      )}
+    </>
+  );
+}
+function App() {
+  return (
+    <>
+      <DataMap />
     </>
   );
 }
