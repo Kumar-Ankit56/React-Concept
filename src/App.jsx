50f0b36b65ba516397a93215eb7749cb4898be11
Ankit Kumar <akg95469@gamil.com>
2023-05-07 00:30:29 +0530

    Basic Form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,52 @@
-import { Route, BrowserRouter, NavLink, Routes } from "react-router-dom";
+import { useState } from "react";
 
-function Home() {
-  return <>Home</>;
-}
-function About() {
-  return <>About Ankit</>;
-}
-function Contact() {
-  return <>Contact</>;
-}
+//Handling Basic form
 function App() {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [age, setage] = useState("");
+
+  const handleName = (e) => {
+    setname(e.target.value);
+  };
+  const handleAge = (e) => {
+    setage(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setemail(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDafault();
+    console.log(name, age, email);
+  };
   return (
     <>
-      <BrowserRouter>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/About">About</NavLink>
-        <NavLink to="/Contact">Contact</NavLink>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Enter Your Name"
+            onChange={handleName}
+          />{" "}
+          <br />
+          <input
+            type="text"
+            value={age}
+            placeholder="Enter Your Age"
+            onChange={handleAge}
+          />{" "}
+          <br />
+          <input
+            type="text"
+            value={email}
+            placeholder="Enter Your Email"
+            onChange={handleEmail}
+          />{" "}
+          <br />
+          <button type="submit">Submit Data</button>
+        </form>
+      </div>
     </>
   );
 }
