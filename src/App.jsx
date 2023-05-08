2587098d0af3c908e48de6f8407a7e03052435fb
Ankit Kumar <akg95469@gamil.com>
2023-05-08 12:20:44 +0530

    Write the text into Clipboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import Home from "./Home";
-//Passing inline css as props
+//Copy text to clipboard
 function App() {
-  const CSSStyle = {
-    color: "red",
-    fontSize: "123px",
-  };
+  var copytext1;
+  function copytext() {
+    copytext1 = document.getElementById("txt");
+    copytext1.select();
+    copytext1.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copytext1.value);
+  }
   return (
     <>
-      <Home style={CSSStyle} />
+      <input type="text" id="txt" placeholder="Please Enter any dummy text" />
+      <button value={copytext1} onClick={copytext}>
+        CopyText
+      </button>
     </>
   );
 }
