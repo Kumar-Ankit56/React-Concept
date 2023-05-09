ca015516513de056b9e988a393bb2748724844a8
Ankit Kumar <akg95469@gamil.com>
2023-05-09 13:37:29 +0530

    Adding active and deactive button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
-//SetTimeout in react component using hook
+import { React, useState } from "react";
+//Create an active and inactive buttons
 function App() {
-  const [hastime, sethastime] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      sethastime(true);
-    }, 5000);
-  });
+  const [box1, setbox1] = useState(false);
+  const [box2, setbox2] = useState(false);
+  const CSSStyle = {
+    color: "red",
+    fontSize: "123px",
+  };
+  const CSSStyle1 = {
+    color: "blue",
+    fontSize: "163px",
+  };
+
+  const ClickBox1 = () => {
+    setbox1(true);
+    setbox2(false);
+  };
+  const ClickBox2 = () => {
+    setbox1(false);
+    setbox2(true);
+  };
   return (
     <>
-      <p>{hastime ? "5 sec passed" : "5 sec has init duration"}</p>
+      <button style={box1 ? CSSStyle : CSSStyle1} onClick={ClickBox1}>
+        Box1
+      </button>
+      <button style={box2 ? CSSStyle1 : CSSStyle} onClick={ClickBox2}>
+        Box2
+      </button>
     </>
   );
 }
