03f3b1f00ee3e5b5a01df452b4dfbe00dad80069
Ankit kumar <akg95469@gamil.com>
2023-06-29 23:11:17 +0530

    Environment setup for ztm with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class App extends Component{
+  render(){
+    return(
+       <h1 className="text-blue-50 font-semibold">Hello world</h1>
+    )
+  }
+}
+export default App
