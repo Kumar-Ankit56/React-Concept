1fe3aea7a3f86855eb2f2fde479eb38560dd930e
Ankit kumar <akg95469@gamil.com>
2023-07-03 00:39:09 +0530

    Set State with class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { Component } from "react";
 
 //Here App is class that is extending the functionalty of component that is givien by react 
-class App extends Component{
+class App extends Component {
   //Render is the method from component]
-  constructor(){
+  constructor() {
     super();
-    this.state={
-       name:"Ankit"
+    this.state = {
+      name: "Ankit"
     }
   }
-  render(){
-    return(<>
-       <h1 className="text-red-700 font-semibold">Hello world</h1>
-       <p className="ml-12 text-blue-500">Hello {this.state.name}</p>
-       <button className="ml-6 w-40 h-10 rounded bg-blue-300 text-white">Click Me</button>
-      </>
+  render() {
+    return (<>
+      <h1 className="text-red-700 font-semibold">Hello world</h1>
+      <p className="ml-12 text-blue-500">Hello {this.state.name}</p>
+      <button className="ml-6 w-40 h-10 rounded bg-blue-300 text-white" onClick={()=>{this.setState({name:"Shivam"})}}>Click Me</button>
+    </>
     )
   }
 }
