8c1d04f7b773025d2755e590b521b9d440f8fb65
Ankit kumar <akg95469@gamil.com>
2023-07-03 00:17:24 +0530

    Class With Construction and basic state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { Component } from "react";
 
 //Here App is class that is extending the functionalty of component that is givien by react 
 class App extends Component{
-  //Render is the method from component
+  //Render is the method from component]
+  constructor(){
+    super();
+    this.state={
+       name:"Ankit"
+    }
+  }
   render(){
-    return(
+    return(<>
        <h1 className="text-red-700 font-semibold">Hello world</h1>
+       <p className="ml-12 text-blue-500">Hello {this.state.name}</p>
+       <button className="ml-6 w-40 h-10 rounded bg-blue-300 text-white">Click Me</button>
+      </>
     )
   }
 }
