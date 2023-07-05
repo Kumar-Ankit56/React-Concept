ec5b9fdb1da2038b9de4c13759b343ad39fb2772
Ankit kumar <akg95469@gamil.com>
2023-07-05 21:16:04 +0530

    set state shallow mearge behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firsname: "Ankit", lastName: "Kumar" },
+      name: "Ankit",
       company: "Centelli",
     };
   }
@@ -15,15 +15,13 @@ class App extends Component {
       <>
         <h1 className="text-red-700 font-semibold">Hello world</h1>
         <p className="ml-12 text-blue-500">
-          Hello {this.state.name.firsname} {this.state.name.lastName} work at{" "}
-          {this.state.company}
+          Hello {this.state.name} work at {this.state.company}
         </p>
         <button
           className="ml-6 w-40 h-10 rounded bg-blue-300 text-white"
           onClick={() => {
-            this.setState(() => {
-              return { name: { firsname: "Ankit", lastName: "Kumar" } };
-            });
+            this.setState({company:"Microsoft"});
+            console.log(this.state);
           }}
         >
           Click Me
